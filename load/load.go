62fda6d3830c072b21eb043ac25420219bec7f9e
Oloniyo Oreoluwa <oreoluwa@Oreoluwas-MacBook-Pro.local>
2023-08-20 20:19:04 +0100

    fixed errors format

--- load/load.go
+++ load/load.go
@@ -11,7 +11,7 @@ func (r *Req) Run() (ResponseData, error) {
 
 	err := r.validate()
 	if err != nil {
-		return ResponseData{}, err
+		return ResponseData{}, fmt.Errorf("r.validate: %w", err)
 	}
 
 	results := make(chan ResponseTime, r.NumberOfRequests)
