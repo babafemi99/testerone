bc1cd4f3f5f89366d802f43461abe679f537037f
Bwise1 <oguntoyebenjamin2@gmail.com>
2023-09-04 10:15:33 +0300

    Added support for both basic and custom configs

--- yaml/yaml.go
+++ yaml/yaml.go
@@ -9,6 +9,14 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type BasicReq struct {
+	NumberOfRequests int           `yaml:"number_of_requests"`
+	URL              string        `yaml:"url"`
+	Interval         int           `yaml:"interval"`
+	RunAfterDuration time.Duration `yaml:"run_after_duration"`
+	RunDuration      int           `yaml:"run_duration"`
+}
+
 type CustomFunction struct {
 	Method  string                 `yaml:"method"`
 	URL     string                 `yaml:"url"`
@@ -39,65 +47,99 @@ type CustomReq struct {
 	RunDuration      int              `yaml:"run_duration"`
 }
 
-func LoadYAMLFile(filePath string) (load.CustomReq, error) {
-	var req CustomReq
+type RequestEntry struct {
+	ReqType       string     `yaml:"req_type"`
+	CustomRequest *CustomReq `yaml:"custom_request"`
+	BasicRequest  *BasicReq  `yaml:"basic_request"`
+}
+
+func LoadYAMLFile(filePath string) (*load.CustomReq, *load.Req, error) {
+	var req []RequestEntry
 
 	yamlContent, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return load.CustomReq{}, err
+		return nil, nil, err
 	}
 
 	err = yaml.Unmarshal(yamlContent, &req)
 	if err != nil {
-		return load.CustomReq{}, err
+		return nil, nil, err
 	}
 
-	var func2Custom []load.CustomFunction
-	for _, cf := range req.Func2 {
+	if len(req) > 0 {
+		entry := req[0]
 
-		bodyJSON, err := json.Marshal(cf.Body)
-		if err != nil {
-			return load.CustomReq{}, err
+		switch entry.ReqType {
+		case "custom":
+
+			if entry.CustomRequest != nil {
+				customReq := *entry.CustomRequest
+				var func2Custom []load.CustomFunction
+				for _, cf := range customReq.Func2 {
+
+					bodyJSON, err := json.Marshal(cf.Body)
+					if err != nil {
+						return nil, nil, err
+					}
+
+					// Convert bodyJSON to byte slice
+					bodyBytes := []byte(bodyJSON)
+
+					// Create headers and cookies slices
+					var headers []load.Header
+					for _, h := range cf.Headers {
+						headers = append(headers, load.Header{
+							Name:  h.Name,
+							Value: h.Value,
+						})
+					}
+
+					var cookies []load.Cookie
+					for _, yc := range cf.Cookies {
+						cookies = append(cookies, load.Cookie{
+							Name:      yc.Name,
+							Value:     yc.Value,
+							ExpiresAt: yc.ExpiresAt,
+						})
+					}
+
+					func2Custom = append(func2Custom, load.CustomFunction{
+						Method:  cf.Method,
+						URL:     cf.URL,
+						Body:    bodyBytes,
+						Headers: headers,
+						Cookies: cookies,
+					})
+				}
+
+				loadReq := &load.CustomReq{
+					// ReqType:          customReq.ReqType,
+					NumberOfRequests: customReq.NumberOfRequests,
+					Interval:         customReq.Interval,
+					Func2:            func2Custom,
+					RunAfterDuration: customReq.RunAfterDuration,
+					RunDuration:      customReq.RunDuration,
+				}
+				return loadReq, nil, nil
+			}
+
+		case "basic":
+			if entry.BasicRequest != nil {
+				basicReq := *entry.BasicRequest
+
+				loadReq := &load.Req{
+					NumberOfRequests: basicReq.NumberOfRequests,
+					URL:              basicReq.URL,
+					Interval:         basicReq.Interval,
+					RunAfterDuration: basicReq.RunAfterDuration,
+					RunDuration:      basicReq.RunDuration,
+				}
+
+				return nil, loadReq, nil
+			}
+
 		}
-
-		// Convert bodyJSON to byte slice
-		bodyBytes := []byte(bodyJSON)
-
-		// Create headers and cookies slices
-		var headers []load.Header
-		for _, h := range cf.Headers {
-			headers = append(headers, load.Header{
-				Name:  h.Name,
-				Value: h.Value,
-			})
-		}
-
-		var cookies []load.Cookie
-		for _, yc := range cf.Cookies {
-			cookies = append(cookies, load.Cookie{
-				Name:      yc.Name,
-				Value:     yc.Value,
-				ExpiresAt: yc.ExpiresAt,
-			})
-		}
-
-		func2Custom = append(func2Custom, load.CustomFunction{
-			Method:  cf.Method,
-			URL:     cf.URL,
-			Body:    bodyBytes,
-			Headers: headers,
-			Cookies: cookies,
-		})
 	}
 
-	loadReq := load.CustomReq{
-		// ReqType:          req.ReqType,
-		NumberOfRequests: req.NumberOfRequests,
-		Interval:         req.Interval,
-		Func2:            func2Custom,
-		RunAfterDuration: req.RunAfterDuration,
-		RunDuration:      req.RunDuration,
-	}
-
-	return loadReq, nil
+	return nil, nil, err
 }
